ea8319eb186771c1099ed0f81bded2c584786730
Onur Koçak <98240992+onurkck34@users.noreply.github.com>
2022-03-27 16:52:09 +0300

    Add files via upload

---
+++ breakandcontinue.py
@@ -0,0 +1,14 @@
+cities = ["Ankara","Istanbul","Izmir"]
+
+for city in cities:
+    if city == "Izmir":
+        break 
+    print(city + " for code = " + city[0:3])
+    print("********")"""
+
+"""names = ["Ahmet","Mehmet","Akif"]
+
+for name in names:
+    if name == "Mehmet":
+        continue 
+    print(name + " For Name = " + name[0:3])
